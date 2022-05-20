71ac5ef92faa72cd78e942d197bb91a1d357f928
Kirill Zhelvakov <blzneci.89@gmail.com>
2022-05-20 23:28:51 +0300

    add chapter_8

---
+++ chapter_8/exercises.py
@@ -0,0 +1,14 @@
+"""
+Упражнения главы 8
+"""
+
+
+poem = '''There was a young lady named Bright.
+Whose speed was far faster than light;
+She started one day
+In a relative way,
+And returned on the previous night.
+'''
+len(poem)
+fout = open('relativity.txt', 'wt')
+fout.write(poem)
